9bc35a0bd08c0e5c8899640ba7492bc5876d3c79
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-12-01 21:31:00 -0300

    2nd test added

--- UnitTest/Services/TodoItemServiceShould.cs
+++ UnitTest/Services/TodoItemServiceShould.cs
@@ -10,6 +10,40 @@ namespace UnitTest.Services
     public class TodoItemServiceShould
     {
         [Fact]
+        public async Task CreateItemAndGetItOK()
+        {
+            var options = new DbContextOptionsBuilder<TodoContext>()
+            .UseInMemoryDatabase(databaseName: "Test_CreateItemAndGetItOK").Options;
+
+            using (var context = new TodoContext(options))
+            {
+                var service = new TodoItemService(context);
+                var item = new NewTodoItemDTO
+                {
+                    Name = "Fake Item",
+                    IsComplete = true
+                };
+
+                var appUser = new ApplicationUser
+                {
+                    Id ="fakeId"
+                };
+
+                var todoItem = await service.CreateAsync(item, appUser);
+            }
+
+            using (var context = new TodoContext(options))
+            {
+                var service = new TodoItemService(context);
+                var item = await service.GetAsync(1L);
+
+                Assert.Equal("Fake Item", item.Name);
+                Assert.Equal("fakeId", item.Responsible.Id);
+                Assert.False(item.IsComplete);
+            }
+        }
+
+        [Fact]
         public async Task CreateWithItemIsIncomplete()
         {
             var options = new DbContextOptionsBuilder<TodoContext>()
